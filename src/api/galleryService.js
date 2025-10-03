e874aca711725de78fec3834b16136c3438f9470
Nisarg <sunisarg05@gmail.com>
2025-10-03 15:33:25 +0530

    Revert "Working Deployment"
    This reverts commit 4cb9727c078c96c8dc4e8d2748ff50472c0ed99d.

--- src/api/galleryService.js
+++ src/api/galleryService.js
@@ -1,3 +1,4 @@
+// This file handles all communication with your backend server.
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:8080';
 
 const apiFetch = async (endpoint, idToken, options = {}) => {
